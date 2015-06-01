9394bb9be1851649ac5fee5a40854b376d50e665
Stefomir <Stefomirrrr@gmail.com>
2015-06-01 15:50:15 +0300

    Web based Calculator
    Web based Calculator(servlets, JDBC, JavaScript, Html, CSS)

--- Calculator/src/com/calculator/Login.java
+++ Calculator/src/com/calculator/Login.java
@@ -9,7 +9,6 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.PrintWriter;
 
-@WebServlet("/Login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
